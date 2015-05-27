fca793a7662fc1abe6f667f910f13148e11ba41d
PedeLa <lange@dbis.rwth-aachen.de>
2015-05-27 11:14:35 +0200

    formatting

--- frontend/graphWidget/js/applicationScript.js
+++ frontend/graphWidget/js/applicationScript.js
@@ -1,22 +1,23 @@
 var client;
 
-var init = function () {
+var init = function() {
 
-  var iwcCallback = function (intent) {
+  var iwcCallback = function(intent) {
     // define your reactions on incoming iwc events here
     console.log(intent);
     // create a node
-    if(intent.action=="CREATE_NODE"){
+    if (intent.action == "CREATE_NODE") {
       var videoDetails = $.parseJSON(intent.data);
       createNode(videoDetails);
     }
-    // send the current graph via IWC so that the load store widget can process it
-    if(intent.action=="STORE_GRAPH"){
+    // send the current graph via IWC so that the load store widget can process
+    // it
+    if (intent.action == "STORE_GRAPH") {
       var graph = getGraph();
       sendGraph(graph);
     }
     // receive the graph, parse the JSON and send it to the graph script
-    if(intent.action=="LOAD_GRAPH"){
+    if (intent.action == "LOAD_GRAPH") {
       var graph = $.parseJSON(intent.data);
       graph.nodes = $.parseJSON(graph.nodes);
       graph.links = $.parseJSON(graph.links);
@@ -27,18 +28,18 @@ var init = function () {
 
 }
 
-var sendPlaybackVideoRequest = function (videoDetails) {
+var sendPlaybackVideoRequest = function(videoDetails) {
   // convert to JSON (one cannot sent JS-arrays via intents)
   videoDetails = JSON.stringify(videoDetails);
   client.sendIntent("PLAYBACK_VIDEO", videoDetails);
 }
 
-var sendGraph = function (graph) {
+var sendGraph = function(graph) {
   // convert to JSON (one cannot sent JS-objects via intents)
   graph = JSON.stringify(graph);
   client.sendIntent("RETURN_GRAPH", graph);
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
   init();
 });
